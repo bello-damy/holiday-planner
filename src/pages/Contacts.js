ad81c4cb3723de42f4addd0f38f533dc54424221
Yussuf Bello <dbello447@gmail.com>
2025-05-02 09:20:56 +0100

    pages structured

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,13 +1,15 @@
 export const Contacts = () => {
     return (
-        <div>
-            <h2>Contact</h2>
-            <ul>
-                <li>Address</li>
-                <li>Phone</li>
-                <li>Email</li>
-                <li>Fax</li>
-            </ul>
+        <div style={{
+            padding: "25px",
+            textAlign: "center",
+            justfyContent: "center"
+        }}>
+            <h2>Contact us</h2>
+            <p> <span style={{ fontWeight: "bold" }}> Address</span> - 111 Street, Central London, England |
+                <span style={{ fontWeight: "bold" }}> Tel</span> - +447-82990-2176 |
+                <span style={{ fontWeight: "bold" }}> Email</span> - info@holidayplanner.co.uk
+            </p>
         </div>
     );
 }
